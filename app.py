f82beb66bf754f8c8fca872b5724cfbb79bee12c
Rochie Cuevas <rochiecuevas@gmail.com>
2018-12-08 01:29:45 -0800

    added scraper function in app.py

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
 # Dependencies for database CRUD
-import pymongo # Use pymongo to allow running MongoDB in Python
+from flask_pymongo import PyMongo # Use flask_pymongo to allow running MongoDB in Python
+import scrape_mars
 
 # Dependencies for rendering the information to HTML
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 # Create an instance for the Flask app
 app = Flask(__name__)
 
 # Connect to a MongoDB database
-conn = "mongodb://localhost:27017"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-# Pass the connection to a pymongo instance
-client = pymongo.MongoClient(conn)
-
-# Connect to a database. If a database of the same name does not exist, it will create a new one.
-db.client.mars_db
-
-# Drop collection if it exists to remove duplicates
-db.information.drop()
-
-# Create a collection in the database containing scraping outputs
-db.information.insert(mars_current_data)
 
 # Call the information in the flask app 
 @app.route('/')
 def index():
     # Store the collection in a list
-    info = list(db.information.find())
-    print(info)
+    info = mongo.db.mars_current_data.find_one()
 
     # Render the template with the information in it
     return render_template("index.html", list = info)
+
+@app.route('/scrape')
+def scraper():
+    info = mongo.db.mars_current_data
+    info_data = scrape_mars.scrape()
+    info.update({}, info_data, upsert = True)
+    return redirect("/", code = 302)
 
 if __name__ == "__main__":
     app.run(debug = True)
